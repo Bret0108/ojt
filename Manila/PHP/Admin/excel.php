6d1262d9de33e079ef8a4a97c388a7e035cccd9b
Joey Chua <joeyochua@gmail.com>
2022-12-01 06:27:02 +0000

    fix #2

--- Manila/PHP/Admin/excel.php
+++ Manila/PHP/Admin/excel.php
@@ -1,12 +1,8 @@
 <?php
+require __DIR__ . '/../../../config.php';
 session_start();
 //Update June 11 2022 Created excel option added office choice option//
 
-$host="localhost";//host name  
-$username="root"; //database username  
-$word="";//database word  
-$db_name="tltd";//database name  
-$tbl_name="reservation"; //table name  
 $con=mysqli_connect("$host", "$username", "$word","$db_name")or die("cannot connect");  
 $output = '';
 $Total = 'Total';
---
+++ config.example.php
@@ -0,0 +1,6 @@
+<?php
+$host="localhost";//host name  
+$username="root"; //database username  
+$word="";//database word  
+$db_name="tltd";//database name  
+$tbl_name="reservation"; //table name  
